a19d8cc6395c986259c5bed2b3bb6ae1c0fca814
email <rrizwan1998@gmail.com>
2023-05-19 20:45:00 +0500

    prediction pipeline and app.py part is done.

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -7,39 +7,112 @@ from tqdm import tqdm
 import tensorflow as tf
 import numpy as np
 from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+# def preprocessing(test_data):
+#     test_len = len(test_data)
+#     test_len = test_len-10
 
-def preprocessing(feature):
-        X = []   
-        img = tf.keras.utils.load_img(feature,target_size=(400,400,3))
-        img = tf.keras.utils.img_to_array(img)
-        img = img/255
-        X.append(img)
-        X= np.array(X)
-        print(X.shape)
-        return X
+#     x_input=test_data[test_len:].reshape(1,-1)
+#     x_input.shape
+       
+#     temp_input=list(x_input)
+#     temp_input=temp_input[0].tolist()
+
+#        # demonstrate prediction for next 10 days
+      
+#     # days = 7
+#     lst_output=[]
+#     n_steps=10
+#     i=0
+#     while(i<7):  
+#         if(len(temp_input)>10):
+#             #print(temp_input)
+#             x_input=np.array(temp_input[1:])
+#             print("{} day input {}".format(i,x_input))
+#             x_input=x_input.reshape(1,-1)
+#             x_input = x_input.reshape((1, n_steps, 1))
+#             #print(x_input)
+#             yhat = model.predict(x_input, verbose=0)
+#             print("{} day output {}".format(i,yhat))
+#             temp_input.extend(yhat[0].tolist())
+#             temp_input=temp_input[1:]
+#             #print(temp_input)
+#             lst_output.extend(yhat.tolist())
+#             i=i+1
+#         else:
+#             x_input = x_input.reshape((1, n_steps,1))
+#             yhat = model.predict(x_input, verbose=0)
+#             print(yhat[0])
+#             temp_input.extend(yhat[0].tolist())
+#             print(len(temp_input))
+#             lst_output.extend(yhat.tolist())
+#             i=i+1
+        
+
+#             print(lst_output)
+#     return lst_output
 
 class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self,features):
+    def predict(self, pred_days: int):
         try:
-            model_path=os.path.join("artifacts","model.h5")
-            model=tf.keras.models.load_model("artifacts\model.h5")
-            test_img=preprocessing(features)
-            preds=model.predict(test_img)
-            preds=np.argmax(preds, axis=1)
-            if preds==0:
-                preds="Business-Finance-Law"
-            elif preds==1:
-                preds="Childrens-Books"
-            else:
-                preds="None"
+            model_path='artifacts\model.h5'
+            test_data=pd.read_csv('./artifacts/test.csv')
+            model=load_object(file_path=model_path)
 
-            return preds
-        
-        except Exception as e:
-            raise CustomException(e,sys)
+            scaler=MinMaxScaler(feature_range=(0,1))
+            test_data=scaler.fit_transform(np.array(test_data).reshape(-1,1))
+
+            test_len = len(test_data)
+            print("actual test_len: ",test_len)
+            test_len = test_len-10
+            print("test_len: ",test_len)
+
+            x_input=test_data[test_len:].reshape(1,-1)
+            x_input.shape
+                        
+            temp_input=list(x_input)
+            temp_input=temp_input[0].tolist()
+
+            # days = 7
+            # self.pred_days = int(pred_days)
+            days = int(pred_days)
+            print("type",type(days))
+            lst_output=[]
+            n_steps=10
+            i=0
+            while(i<days):
+                if(len(temp_input)>10):
+                    #print(temp_input)
+                    x_input=np.array(temp_input[1:])
+                    print("{} day input {}".format(i,x_input))
+                    x_input=x_input.reshape(1,-1)
+                    x_input = x_input.reshape((1, n_steps, 1))
+                    #print(x_input)
+                    yhat = model.predict(x_input, verbose=0)
+                    print("{} day output {}".format(i,yhat))
+                    temp_input.extend(yhat[0].tolist())
+                    temp_input=temp_input[1:]
+                    #print(temp_input)
+                    lst_output.extend(yhat.tolist())
+                    i=i+1
+                else:
+                    x_input = x_input.reshape((1, n_steps,1))
+                    yhat = model.predict(x_input, verbose=0)
+                    print(yhat[0])
+                    temp_input.extend(yhat[0].tolist())
+                    print(len(temp_input))
+                    lst_output.extend(yhat.tolist())
+                    i=i+1
+    
+            GRU_df3 = scaler.inverse_transform(lst_output)
+            print("Future 7 days close price prediction for GRU: ", GRU_df3)
+# print(lst_output)
+            return GRU_df3
+        # except Exception as e:
+        #     raise CustomException(e,sys)        
         
 # class CustomData:
 #     def __init__(self,img_path):
@@ -88,7 +161,7 @@ class PredictPipeline:
 #                 "thal":[self.thal]
 #             }
 
-#             return pd.DataFrame(custom_data_input_dict)
+            # return pd.DataFrame(custom_data_input_dict)
     
-        # except Exception as e:
-        #     raise CustomException(e,sys)
+        except Exception as e:
+            raise CustomException(e,sys)
